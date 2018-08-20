ec9014dc3c8206e97c5f8cfff2c3098fca0346b1
Ishaan Banerjee <ishaanb98@gmail.com>
2018-08-21 00:54:18 +0530

    Firebase logins done

--- app/src/main/java/com/example/newu/exa_ui/MainActivity.java
+++ app/src/main/java/com/example/newu/exa_ui/MainActivity.java
@@ -9,6 +9,15 @@ import android.view.MenuItem;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bnv;
@@ -18,12 +27,16 @@ public class MainActivity extends AppCompatActivity {
     About b;
     Setting s;
 
+    private static final int RC_SIGN_IN = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bnv=findViewById(R.id.main_nav);
+        authentication();
+
+        //bnv=findViewById(R.id.main_nav);
         fr=findViewById(R.id.main_frame);
 
         c = new client();
@@ -31,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         b=new About();
         s=new Setting();
 
+        /*
         bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -58,7 +72,48 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        */
 
+    }
+
+    private void authentication() {
+
+        // Choose authentication providers
+        List providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                Toast.makeText(getApplicationContext(), "Sign Successful", Toast.LENGTH_SHORT).show();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
